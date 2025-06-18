ea1f8e9b98350824ed22d995b002e4484651cfca
José Antonio <jose_antoniorivera@outlook.com>
2025-06-17 22:21:49 -0600

    build: create dev and prod profiles

--- src/main/java/org/puzre/GreetingResource.java
+++ src/main/java/org/puzre/GreetingResource.java
@@ -4,13 +4,17 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name = "message.profile")
+    String message;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return "Hello from " + message;
     }
 }
